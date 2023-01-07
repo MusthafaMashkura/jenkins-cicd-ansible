446bfddc4b8aa81c4c291123390c257632133e9e
MusthafaMashkura <47938530+MusthafaMashkura@users.noreply.github.com>
2023-01-07 19:32:03 +0530

    Update jenkins_demo.java

--- src/test/java/example/jenkins_demo.java
+++ src/test/java/example/jenkins_demo.java
@@ -7,3 +7,23 @@ import org.openqa.selenium.chrome.ChromeDriver;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+public class jenkins_demo
+{
+ 
+@Test
+public void testgooglesearch(){
+ 
+WebDriver driver = new ChromeDriver();
+//it will open the goggle page
+driver.get("http://google.in"); 
+//we expect the title “Google “ should be present 
+String Expectedtitle = "Google";
+//it will fetch the actual title 
+String Actualtitle = driver.getTitle();
+System.out.println("Before Assetion " + Expectedtitle + Actualtitle);
+//it will compare actual title and expected title
+Assert.assertEquals(Actualtitle, Expectedtitle);
+//print out the result
+System.out.println("After Assertion " + Expectedtitle + Actualtitle + " Title matched ");
+ }
+}
