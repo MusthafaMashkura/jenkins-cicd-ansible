7c8226a51a4d05649ac9c4cb0454f6d31d495446
MusthafaMashkura <47938530+MusthafaMashkura@users.noreply.github.com>
2023-01-09 14:03:15 +0530

    Create selenium-test.java

---
+++ selenium-test.java
@@ -0,0 +1,31 @@
+import java.io.IOException;
+ 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.Test;
+ 
+public class TecAdminSeleniumTest {
+ 
+        public static void main(String[] args) throws IOException, InterruptedException {
+                System.setProperty("webdriver.chrome.driver", "/usr/bin/chromedriver");
+                ChromeOptions chromeOptions = new ChromeOptions();
+                chromeOptions.addArguments("--headless");
+                chromeOptions.addArguments("--no-sandbox");
+ 
+                WebDriver driver = new ChromeDriver(chromeOptions);
+ 
+         //       driver.get("https://google.com");
+                driver.get("http://13.232.140.244:8080/hello-world-maven/");
+ 
+                Thread.sleep(1000);
+ 
+         //       if (driver.getPageSource().contains("I'm Feeling Lucky")) {
+                if (driver.getPageSource().contains("ansible roles")) {
+                        System.out.println("Pass");
+                } else {
+                        System.out.println("Fail");
+                }
+                driver.quit();
+        }
+}
