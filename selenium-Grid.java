1d61dc5963a09909fdb3ffc19eee395c46ce75c9
MusthafaMashkura <47938530+MusthafaMashkura@users.noreply.github.com>
2023-01-12 07:52:55 +0530

    Create selenium-Grid.java

---
+++ selenium-Grid.java
@@ -0,0 +1,7 @@
+import org.openqa.selenium.*; 
+import org.openqa.selenium.remote.DesiredCapabilities; 
+import java.net.MalformedURLException; 
+import java.net.URL; 
+import org.openqa.selenium.remote.RemoteWebDriver; 
+import org.testng.Assert;
+import org.testng.annotations.*;
