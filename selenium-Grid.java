34e6b7b9b03584fb5c045ce5c314a22246279e84
MusthafaMashkura <47938530+MusthafaMashkura@users.noreply.github.com>
2023-01-12 08:55:55 +0530

    Update selenium-Grid.java

--- selenium-Grid.java
+++ selenium-Grid.java
@@ -8,7 +8,7 @@ import java.util.logging.*;
 //import log4j;
 //import org.apache.logging.log4j.LogManager;
 //import org.apache.logging.log4j.Logger;
-//import org.apache.logging.log4j.*;
+import org.apache.logging.log4j.*;
 import org.testng.Assert;
 import org.testng.annotations.*;
 
