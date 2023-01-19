2f8878915f2fcf42867b1f12ef60370db992ad67
MusthafaMashkura <47938530+MusthafaMashkura@users.noreply.github.com>
2023-01-19 12:31:27 +0530

    Update git.groovy

--- vars/git.groovy
+++ vars/git.groovy
@@ -1,5 +1,5 @@
 def git() {
-/def call(String repourl) {
+//def call(String repourl) {
 git branch: 'jenkins-selenium-grid', url: 'https://github.com/MusthafaMashkura/jenkins-cicd-ansible.git'
  // git branch: 'jenkins-selenium-grid', url:"${repourl}"
 }
